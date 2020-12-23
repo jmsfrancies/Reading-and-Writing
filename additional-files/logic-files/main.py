eaa732ab78db37218aa87559201c10b854b25cbd
James Francies <jfrancies1@live.maryville.edu>
2020-12-23 10:12:04 -0600

    Modified the Slide Show to improve code readability

--- additional-files/logic-files/main.py
+++ additional-files/logic-files/main.py
@@ -1,150 +1,32 @@
 from graphics import *
 import time
 
-
-# Disjuncitve Syllogism Slide
-def Disjunctive(win, x, y):
-    Title = Text(Point(350, 150), "Disjunctive Syllogism")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    Disjunctive = Text(Point(350, 250), ("""{0} v {1}
-~{0},
-{1}""".format(x, y)))
-    Disjunctive.setTextColor("black")
-    Disjunctive.setSize(22)
-    Title.draw(win)
-    Disjunctive.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    Disjunctive.undraw()
-
-# Modus Ponens Slide
+# Slide show function
 
 
-def ModusPonens(win, x, y):
-    Title = Text(Point(350, 150), "Modus Ponens")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    ModusPonens = Text(Point(350, 250), """{0} => {1}
-{0}
-{1}""".format(x, y))
-    ModusPonens.setTextColor("black")
-    ModusPonens.setSize(22)
-    ModusPonens.draw(win)
-    Title.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    ModusPonens.undraw()
+def slide_show(win):
+    # slide show dictionary that stores the slides required for the presentation.
+    slide_show_dict = {"Disjunctive Syllogism": "P v Q\n~ P\nQ", "Modus Ponens": "P => Q\nP \nQ",
+                       "Modus Tollens": "P => Q\n~ Q \n~ P", "Conjunction": "P & Q\nP\nQ",
+                       "Material Implication": "~ P v Q\nP => Q", "Hypothectical Syllogism": "P=>Q\nQ=>R\nP=>R",
+                       "Dilemma": "P v Q\nP => R\nQ => S\nR v S"}
 
-# Modus Tollens Slide
-
-
-def ModusTollens(win, x, y):
-    Title = Text(Point(350, 150), "Modus Tollens")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    ModusTollens = Text(Point(350, 250), """{0} => {1}
-~{1}
-~{0}""".format(x, y))
-    ModusTollens.setTextColor("black")
-    ModusTollens.setSize(22)
-    Title.draw(win)
-    ModusTollens.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    ModusTollens.undraw()
-
-# Conjunction Slide
-
-
-def Conjunction(win, x, y):
-    Title = Text(Point(350, 150), "Conjunction")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    Conjunction = Text(Point(350, 250), """{0} & {1}
-{0}
-{1}""".format(x, y))
-    Conjunction.setTextColor("black")
-    Conjunction.setSize(22)
-    Title.draw(win)
-    Conjunction.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    Conjunction.undraw()
-
-# Hypothetical Syllogism Slide
-
-
-def Hypothetical(win, x, y, z):
-    Title = Text(Point(350, 150), "Hypothetical Syllogism")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    Hypothetical = Text(Point(350, 250), """{0} => {1}
-{1} => {2}
-{0} => {2}""".format(x, y, z))
-    Hypothetical.setTextColor("black")
-    Hypothetical.setSize(22)
-    Title.draw(win)
-    Hypothetical.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    Hypothetical.undraw()
-
-# Material Implication Syllogism Slide
-
-
-def Material_Implication(win, x, y):
-    Title = Text(Point(350, 150), "Material Implication")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    Material = Text(Point(350, 250), """(~{0} v {1})
-({0} => {1})""".format(x, y))
-    Material.setTextColor("black")
-    Material.setSize(22)
-    Title.draw(win)
-    Material.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    Material.undraw()
-    Title = Text(Point(350, 150), "Dilemma")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-
-# Dilemma Slide
-
-
-def Dilemma(win, x, y, z, a):
-    Title = Text(Point(350, 150), "Dilemma")
-    Title.setTextColor("blue")
-    Title.setSize(20)
-    Dilemma = Text(Point(350, 250), """{0} v {1}
-{0} => {2}
-{1} => {3}
-{2} v {3}
-""".format(x, y, z, a))
-    Dilemma.setTextColor("black")
-    Dilemma.setSize(22)
-    Title.draw(win)
-    Dilemma.draw(win)
-    time.sleep(2)
-    Title.undraw()
-    Dilemma.undraw()
-    Title = Title = Text(
-        Point(350, 150), "Enjoy having a logic filled life! ")
-    Title.setTextColor("blue")
-    Title.setSize(30)
-    Title.draw(win)
-    time.sleep(2)
-    win.close()
+    # for loop that prints the slides onto the graphics window.
+    for i, value in slide_show_dict.items():
+        Title = Text(Point(350, 150), i)
+        Title.setTextColor("blue")
+        Title.setSize(20)
+        Body = Text(Point(350, 250), value)
+        Body.setTextColor("black")
+        Body.setSize(22)
+        Title.draw(win)
+        Body.draw(win)
+        time.sleep(2)
+        Title.undraw()
+        Body.undraw()
 
 
 def main():
-    # Logic Variables for the logic slides powerpoint
-    x = "A"
-    y = "B"
-    z = "C"
-    a = "D"
-
     # Win object that creates the window and background color.
     win = GraphWin("Logic, Logic, Logic!", 700, 500)
     win.setBackground(color_rgb(200, 200, 200))
@@ -190,15 +72,9 @@ ENJOY!""".format(first_name_inputText.getText(), last_name_inputText.getText()))
     time.sleep(1)
     Slogan.undraw()
     time.sleep(2)
-
-    # Listed out functions for each slide utilized for the presentation
-    Disjunctive(win, x, y)
-    ModusPonens(win, x, y)
-    ModusTollens(win, x, y)
-    Hypothetical(win, x, y, z)
-    Conjunction(win, x, y)
-    Material_Implication(win, x, y)
-    Dilemma(win, x, y, z, a)
+    slide_show(win)
+    # Slide Show Function for the presentation
+    slide_show(win)
 
 
 main()
