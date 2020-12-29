9f33e8178bc2500167a2a063c2de2a2b828fc5ce
James Francies <jfrancies1@live.maryville.edu>
2020-12-28 20:16:51 -0600

    hello world that outputs on a terminal like a staircase with colorama for a more unique hello world experience

--- additional-files/helloworld.py
+++ additional-files/helloworld.py
@@ -1,7 +1,9 @@
+from colorama import Fore,Style,Back
 def main():
     space = " "
     for i in "HelloWorld!":
-        print("{0}{1}".format(space,i))
-        space += " "
-    
+        print(Back.GREEN)
+        print(Style.BRIGHT + Fore.RED + "{0}{1}".format(space,i))
+        space = space + " "
+    print(Style.RESET_ALL)
 main()
