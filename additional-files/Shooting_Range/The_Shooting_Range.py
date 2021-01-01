6d6ac4ab8fe9b7a1314ae2316d2010e208cf7ae9
James Francies <jfrancies1@live.maryville.edu>
2020-12-31 19:52:49 -0600

    Modified this program for the handgun shooting for now.

--- additional-files/Shooting_Range/The_Shooting_Range.py
+++ additional-files/Shooting_Range/The_Shooting_Range.py
@@ -6,14 +6,17 @@ import time
 
 
 def Ready(win, title):
+    # for loop to print each target ring onto the graphics window
     total = 0
     target_color = ["Blue", "Green", "Yellow", "Orange", "Red"]
     for i in reversed(range(30, 171, 35)):
         Target = Circle(Point(250, 250), i)
+        #Target.setfill is formatted to be one of the five colors on the target color list
         Target.setFill("{0}".format(target_color[total]))
         Target.draw(win)
         total = total + 1
 
+    # Where the gunshots display
     for i in range(25):
         BulletHole = win.getMouse()
         x = BulletHole.getX()
@@ -29,17 +32,15 @@ def Ready(win, title):
     win.getMouse()
     win.close()
 
-
+# Main Function that houses the graphics window and Ready function call
 def main():
     win = GraphWin("The Shooting Range", 500, 500)
     win.setBackground("white")
-    title = Text(Point(250, 25), """Welcome to The Machine Gun Shooting Range!
+    title = Text(Point(250, 25), """Welcome to The Handgun Shooting Range!
 Where Precision and accuracy get further refined!""")
     title.setSize(20)
     title.setStyle("bold italic")
     title.draw(win)
-    HowManyRounds = 30
-    WhatCaliber = 1
     win.getMouse()
     Ready(win, title)
 
